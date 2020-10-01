b75ee78feb703c944110e095dcee348bcc486df0
Thomas Nylund <68823316+styr0x@users.noreply.github.com>
2020-10-01 08:57:55 +0300

    Väderapp
    Funkar

--- Program/weather/script.js
+++ Program/weather/script.js
@@ -1,10 +1,26 @@
 let displayTemp = document.getElementById("temp");
+let commentText = document.getElementById("comment");
 
-fetch(`api.openweathermap.org/data/2.5/weather?id=656130&appid=fb3069e2cddf22b9c087ca74ea3e2aba`)
-.then(res => res.json)
+fetch(`http://api.openweathermap.org/data/2.5/weather?id=656130&appid=fb3069e2cddf22b9c087ca74ea3e2aba&units=metric`)
+.then(res => res.json())
 .then (onWeatherLoaded);
 
 
 function onWeatherLoaded(data) {
     console.log(data);
+displayTemp.innerText = `${data.main.temp} °C`
+
+if (data.main.temp < 0) {
+    commentText.innerText = "Brrr, it's cold as fuck man!"
+}
+else if (data.main.temp <= 15){
+    commentText.innerText = "Better put a jacket on!"
+}
+else if (data.main.temp > 15) {
+    commentText.innerText = "It's pretty alright!"
+}
+else if (data.main.temp > 25) {
+    commentText.innerText = "Fuck, it's hot!"
+} 
+
 }
