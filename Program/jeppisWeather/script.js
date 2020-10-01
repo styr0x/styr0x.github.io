9d6b57d55209b55886eaab0a137b2e47017b7653
Thomas Nylund <68823316+styr0x@users.noreply.github.com>
2020-10-01 09:40:49 +0300

    asd
    dsa

---
+++ Program/jeppisWeather/script.js
@@ -0,0 +1,26 @@
+let displayTemp = document.getElementById("temp");
+let commentText = document.getElementById("comment");
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?id=656130&appid=fb3069e2cddf22b9c087ca74ea3e2aba&units=metric`)
+.then(res => res.json())
+.then (onWeatherLoaded);
+
+
+function onWeatherLoaded(data) {
+    console.log(data);
+displayTemp.innerText = `${data.main.temp} °C`
+
+if (data.main.temp < 0) {
+    commentText.innerText = "Brrr, it's cold as fuck man!"
+}
+else if (data.main.temp <= 15){
+    commentText.innerText = "Better put a fucking jacket on!"
+}
+else if (data.main.temp > 15) {
+    commentText.innerText = "It's pretty fucking alright!"
+}
+else if (data.main.temp > 25) {
+    commentText.innerText = "Fuck, it's hot!"
+} 
+
+}
