94b3d8300719149e52c34ac35a7deeb71f310e63
Thomas Nylund <68823316+styr0x@users.noreply.github.com>
2020-10-05 12:19:31 +0300

    Guitarshit

--- Program/chordVisualizer/script.js
+++ Program/chordVisualizer/script.js
@@ -1,9 +1,24 @@
-let neckDiv = document.getElementById("neck")
+let neckScale = 90 * 2;
+let firstFret = neckScale / 17.817;
+
+
+let parentDiv = document.getElementById("parent");
+
 let firstFretDistance = "10.103vw"
 let frets = ["19.638", "28.639", "37.134", "45.152", "52.721", "59.864", "66.607", "72.971", "78.978", "84.648", "90.00", "95.051"];
 
 
-for (let i = 0; i < 13; i++) {
+makeNeck();
+
+
+function makeNeck(){
+    let neck = document.createElement("div");
+    neck.id = `neck`
+    neck.className = `neck`
+    parentDiv.appendChild(neck);
+
+for (let i = 0; i < 12; i++) {
+    let neckDiv = document.getElementById("neck");
     let fret = document.createElement("div");
     fret.id = `fret${i}`
     fret.className = "fret"
@@ -12,4 +27,4 @@ for (let i = 0; i < 13; i++) {
     console.log(i)
 }
 
-
+}
