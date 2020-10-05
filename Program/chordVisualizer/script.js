42ff574d3918a2eae80e93f1c7fd9705c751645b
Thomas Nylund <68823316+styr0x@users.noreply.github.com>
2020-10-05 09:41:16 +0300

    Guitar neck ratio
    Såja

---
+++ Program/chordVisualizer/script.js
@@ -0,0 +1,15 @@
+let neckDiv = document.getElementById("neck")
+let firstFretDistance = "10.103vw"
+let frets = ["19.638", "28.639", "37.134", "45.152", "52.721", "59.864", "66.607", "72.971", "78.978", "84.648", "90.00"];
+
+
+for (let i = 0; i < 12; i++) {
+    let fret = document.createElement("div");
+    fret.id = `fret${i}`
+    fret.className = "fret"
+    fret.style.transform=`translate(${frets[i]}vw,0)`
+    neckDiv.appendChild(fret);
+    console.log(i)
+}
+
+
