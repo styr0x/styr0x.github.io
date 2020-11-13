e6c2603cb42535738535823642fdca7fc52174b0
Thomas Nylund <68823316+styr0x@users.noreply.github.com>
2020-11-13 11:03:28 +0200

    Scoredisplay

--- Spel/pong/utility/score.js
+++ Spel/pong/utility/score.js
@@ -1,22 +1,41 @@
 import {ai, ball, player} from '../game.js';
+import {gameWindow} from '../gamewindow.js';
 //Element
 const playerScore = document.getElementById("playerOneScoreId");
 const aiScore = document.getElementById("playerTwoScoreId");
-const messager = document.getElementById("announcer");
+const messenger = document.getElementById("announcer");
 //Variabler
 let playerScoreCount = 0;
 let aiScoreCount = 0;
 //Funktioner
 export function scoreUpPlayer() {
-    playerScoreCount++
+    messenger.innerHTML = "YOU SCORE!";
+    messenger.className = "titleClassAnimated";
+    playerScoreCount++;
     playerScore.innerText = playerScoreCount;
+    playerScore.className = "titleClassAnimated";
+    setTimeout(function (){
+        messenger.innerHTML = "PONG";
+        messenger.className = "titleClass";
+        playerScore.className = "scoreClassPlayer";
+    
+    } ,2000)
     resetBall(-5);
 }
 
 export function scoreUpAi() {
+    messenger.innerHTML = "AI SCORES!"
+    messenger.className = "titleClassAnimated";
     aiScoreCount++
     aiScore.innerText = aiScoreCount;
+    aiScore.className = "titleClassAnimated";
+    setTimeout(function (){
+        messenger.innerHTML = "PONG"
+        messenger.className = "titleClass";
+        aiScore.className = "scoreClassPlayer";
+    }, 2000)
     resetBall(5);
+
 }
 
 function resetBall(direction) {
